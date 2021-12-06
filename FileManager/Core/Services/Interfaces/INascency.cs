0adc8a3d132f0a9dbd9024d56faf550c52362791
Дмитрий <semu-dima@yandex.ru>
2021-12-06 22:26:03 +0300

    переименовал интерфейс

---
+++ FileManager/Core/Services/Interfaces/INascency.cs
@@ -0,0 +1,12 @@
+﻿namespace FileManager.Services.Interfaces
+{
+    public interface INascency<T> where T : new()
+    {
+        static T? _instance;
+        static T Instance()
+        {
+            if (_instance is null) _instance = new();
+            return _instance;
+        }
+    }
+}
