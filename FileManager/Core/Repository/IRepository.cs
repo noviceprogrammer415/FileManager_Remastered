7522c048a5aed98c4e4ca9ee0bc490e779ce1c03
Дмитрий <semu-dima@yandex.ru>
2021-12-06 22:25:36 +0300

    добавил репозиторий

---
+++ FileManager/Core/Repository/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace FileManager.Core.Repository
+{
+    public interface IRepository
+    {
+        void Create(string path);
+        void Read();
+        void Update(string path);
+    }
+}
---
+++ FileManager/Core/Repository/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.Core.Repository
+{
+    public class Repository : IRepository
+    {
+        public void Create(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
